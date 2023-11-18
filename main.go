49c54d4c98ac99623cef71a25740c7dcc38405f0
ciper <anmolkarki101@gmail.com>
2023-11-18 11:32:22 +0545

    feat: Implement POST endpoint for creating new employees
    This commit adds a new route for handling POST requests to /employee. It parses the request body into an Employee struct, sets the ID to an empty string, inserts the employee into the MongoDB employees collection, and returns the newly created employee as a JSON response. Error handling is included to manage potential issues during request parsing, database insertion, and document retrieval.

--- main.go
+++ main.go
@@ -88,8 +88,27 @@ func main() {
 		return c.JSON(employees)
 	})
 
-	app.Post("/employee")
-	app.Put("/employee/:id")
+	app.Post("/employee", func(c *fiber.Ctx) error {
+		collection := mg.Db.Collection("employees")
+		employee := new(Employee)
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+		employee.ID = ""
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+		createdRecord := collection.FindOne(c.Context(), filter)
+		createdEmployee := &Employee{}
+		createdRecord.Decode(createdEmployee)
+		return c.Status(201).JSON(createdEmployee)
+
+	})
+	app.Put("/employee/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+	})
 
 	app.Delete("/employee/id")
 }
